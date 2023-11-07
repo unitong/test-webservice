811cf9a46ec2d25ca05cf52708087dbc925fcfc2
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-11-07 19:28:09 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,27 +7,3 @@ output "ports" {
   description = "Service Ports"
   value       = var.ports
 }
-
-output "test_module" {
-  description = "Testing module output"
-  value       = var.test_module
-}
-
-output "test_var" {
-  description = "Testing var"
-  value = [
-    {
-      name: "system_disk_size",
-      value: var.system_disk_size,
-      test_list: var.test_list,
-      test_mix: var.test_mix,
-      test_mix_number: var.test_mix_number
-      test_number_list: var.test_number_list
-    },
-    {
-      name: "test2",
-      value: var.test_options
-    }
-  ]
-
-}
